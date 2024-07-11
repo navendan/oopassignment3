6568adb0e504ecba70d35b1e07ab786038f43503
navendan <126914145+navendan@users.noreply.github.com>
2024-07-11 15:09:47 -0600

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,15 @@
+student_id=[]
+student_first_name=[]
+student_last_name=[]
+student_gpa=[]
+
+def print_menu():
+    
+
+def display_std_header():
+    print("""
+    ---------------------------------------------
+        student_id, student_name, student_gpa
+    ---------------------------------------------      
+     """     )
+def display_student():
