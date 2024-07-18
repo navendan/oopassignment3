58055b8427d36304ff55295eec2e22979c306439
Krisotf <kristofanemi@gmail.com>
2024-07-18 15:08:15 -0600

    Update assignment3.py
    Added the function to print character lines, and the skeleton of the functions for display student (individual) and list students (all). Add students function needs to error if a duplicate ID is submitted

--- assignment3.py
+++ assignment3.py
@@ -3,6 +3,46 @@ student_first_name=[]
 student_last_name=[]
 student_gpa=[]
 
+def printLineOfChars(char, repeats):
+    line = str(char) * repeats
+    print(line)
+
+def display_student(id):
+    students_list_file = open('students.txt', 'r')
+    for line in students_list_file:
+        student_list = line.rstrip().split(',')
+        student_id = student_list[0]
+        student_name = student_list[1]
+        student_gpa = float(student_list[2])
+    # need to determine how to point at the exact student using just the ID as input
+    # display_std_header()
+    print("{:<20} {:<20} {:<10.0f}" .format(id, student_name, student_gpa))
+    printLineOfChars('=', 75)
+    students_list_file.close()
+
+def list_students():
+    students_list_file = open('students.txt', 'r')
+    #If received list is empty, print “Students list has no students” error message.
+    # display_std_header()
+    for line in students_list_file:
+        student_list = line.rstrip().split(',')
+        student_id = student_list[0]
+        student_name = student_list[1]
+        student_gpa = float(student_list[2])
+        print("{:<20} {:<20} {:<10.0f}" .format(student_id, student_name, student_gpa))
+    printLineOfChars('=', 75)
+
+def add_students():
+    students_list_file = open('students.txt', 'a')
+    print ("Adding a student...")
+    new_student_id = input("Please enter the student ID: ")
+    # Need to write an error for 'Student already exists with the given ID'.
+    new_student_name = input("Please enter the student's name: ")
+    new_student_gpa = (input("Please enter the student's GPA: "))
+    students_list_file.write('\n' + new_student_id + ',' + new_student_name + ',' + new_student_gpa)
+    print(f"Student {new_student_name} added successfully.")
+    students_list_file.close()
+
 def edit_student():
     edit_student_id = int(input("Enter student ID to edit: "))
     if edit_student_id == student_id:
